4ba652e7a1d7d52e241e645cac9a784914cfd325
Евген <62559389+EugenKoulik@users.noreply.github.com>
2023-03-25 13:16:24 +0300

    Added dz2

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,41 @@
 <script setup>
-import Timer from "./components/Timer.vue";
+import CardList from "./components/CardList.vue";
+import css from "./App.module.css";
+import { reactive, ref, computed } from "vue";
+
+const input = ref("");
+
+const cards = reactive([
+  {
+    id: 1,
+    name: "Anton",
+    description: "QA",
+    count: 1200
+  },
+  {
+    id: 2,
+    name: "Bogdan",
+    description: "Middle",
+    count: 1800
+  },
+  {
+    id: 3,
+    name: "Denis",
+    description: "Junior",
+    count: 800
+  }
+]);
+
+const filteredCards = computed(() => {
+  return cards.filter(card => card.name.includes(input.value))
+});
+
 </script>
 
 <template>
-  <Timer></Timer>
+  <div :class="css.app">
+    <input v-model="input"/>
+    <CardList :cards="filteredCards"/>
+  </div>
 </template>
 
---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<script setup>
+import css from "../App.module.css";
+
+defineProps({
+  name: String,
+  description: String,
+  count: Number
+});
+</script>
+
+<template>
+ <div :class="css.card">
+   <div><strong>Имя : </strong> {{name}} </div>
+   <div><strong>Описание : </strong> {{description}} </div>
+   <div><strong>Количество : </strong> {{count}} </div>
+ </div>
+</template>
